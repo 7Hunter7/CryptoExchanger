55532245980168b30bd94e3333bbbaf3342fbb71
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-05 20:22:26 +0300

    Updated component 'InputCrypto' - Added button and styles for it

--- src/components/InputCrypto.vue
+++ src/components/InputCrypto.vue
@@ -8,6 +8,8 @@
     @input="changeAmount($event.target.value)"
     placeholder="Введите число"
   />
+  <br />
+  <button class="button">Конвертировать</button>
 </template>
 
 <script setup>
@@ -36,6 +38,27 @@ defineProps({
   &:focus {
     outline: none;
     border-color: var(--color-primary);
+  }
+}
+.button {
+  margin-top: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-md);
+  border: none;
+  border-radius: var(--spacing-xs);
+  color: var(--color-primary);
+  text-shadow: var(--text-shadow);
+  background-color: var(--background-select-color);
+  font-family: var(--font-family-secondary);
+  font-size: var(--font-size-item);
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: var(--color-black);
+    background-color: var(--color-primary);
+  }
+  &:active {
+    transition: transform 0.1s ease-in-out;
+    transform: scale(0.97);
   }
 }
 </style>
