23f0e85fb3c3d168d64a3fcdf60ed3e791293c5f
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-05 18:55:31 +0300

    Created new component 'SelectCrypto'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>Crypto exchanger</h1>
   <input-crypto />
+  <select-crypto />
 </template>
 
 <script setup>
 import InputCrypto from '@/components/InputCrypto.vue'
+import SelectCrypto from '@/components/SelectCrypto.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/SelectCrypto.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="list">
+    <li class="list__item">Выберите валюту</li>
+    <li class="list__item">Bitcoin</li>
+    <li class="list__item">ETH</li>
+    <li class="list__item">USDT</li>
+  </ul>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+.list {
+  margin-top: var(--spacing-md);
+  padding: 0;
+  width: var(--input-width);
+  list-style: none;
+  color: var(--color-primary);
+  border: 2px solid var(--color-black);
+  border-radius: var(--spacing-md);
+
+  &__item {
+    padding: var(--spacing-xxs) var(--spacing-xs);
+    font-size: var(--font-size-base);
+    margin-bottom: var(--spacing-xxs);
+    transition: transform 0.2s ease-in-out;
+    cursor: pointer;
+    &:hover {
+      text-shadow: var(--text-shadow);
+    }
+    &:active {
+      transform: scale(0.95);
+    }
+  }
+}
+</style>
