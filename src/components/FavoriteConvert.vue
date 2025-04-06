e773debfcf60330adc2b45127e223a010d79c16c
Hunter <i.am.hunter.777.ih@gmail.com>
2025-04-06 12:41:23 +0300

    Updated 'main.css' - Added button styles/ Updated component 'InputCrypto' - Updated styles using it'. Updated component  'FavoriteConvert' - Updated template with 'favourite', 'favourite__list', 'favourite__item', 'favourite__button' and updated styles using them

--- src/components/FavoriteConvert.vue
+++ src/components/FavoriteConvert.vue
@@ -1,9 +1,77 @@
 <template>
-  <div class="favorite-convert">
+  <div class="favourite">
     <h2>Избранное</h2>
+    <div v-if="favourites.length > 0" class="favourite__list">
+      <ul>
+        <li v-for="(item, index) in favourites" :key="index" class="favourite__item">
+          <span>{{ item.from }}</span>
+          <span>→</span>
+          <span>{{ item.to }}</span>
+          <button @click="removeFavourite(index)" class="favourite__button">Удалить</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  favourites: {
+    type: Array,
+    required: true,
+  },
+})
+defineEmits(['removeFavourite'])
+</script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.favourite {
+  width: var(--selectors-width);
+  margin: var(--spacing-md) auto;
+  padding: var(--spacing-md);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: var(--spacing-xs);
+  border: 2px solid var(--color-black);
+  border-radius: var(--spacing-md);
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  h2 {
+    color: var(--color-text-primary);
+    text-shadow: var(--text-shadow);
+    text-transform: uppercase;
+    font-weight: var(--font-weight-semibold);
+    font-size: var(--font-size-title);
+  }
+  &__list {
+    list-style-type: none;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: var(--spacing-sm);
+  }
+  &__item {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    transition: background-color 0.3s ease;
+    &:hover {
+      background-color: var(--background-select-color);
+    }
+  }
+  &__button {
+    cursor: pointer;
+    color: var(--color-whit);
+    background-color: var(--color-red);
+    &:hover {
+      background-color: var(--color-red), 10%;
+    }
+    &:active {
+      background-color: var(--color-red), 20%;
+    }
+  }
+}
+</style>
--- src/components/InputCrypto.vue
+++ src/components/InputCrypto.vue
@@ -47,24 +47,6 @@ defineProps({
 }
 .button {
   margin-top: var(--spacing-md);
-  padding: var(--spacing-xs) var(--spacing-md);
-  border: none;
-  border-radius: var(--spacing-xs);
-  color: var(--color-primary);
-  text-shadow: var(--text-shadow);
-  background-color: var(--background-select-color);
-  font-family: var(--font-family-secondary);
-  font-size: var(--font-size-item);
-  cursor: pointer;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    color: var(--color-black);
-    background-color: var(--color-primary);
-  }
-  &:active {
-    transition: transform 0.1s ease-in-out;
-    transform: scale(0.97);
-  }
 }
 .favorite-button {
   margin-left: var(--spacing-md);
